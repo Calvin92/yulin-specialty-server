875c0cfed38b419bd6eed7f6f7f7285c76d0c104
阮嘉伟 <ruanjiawei1@jd.com>
2018-10-14 00:55:49 +0800

    Update: 配置项目

---
+++ model/user.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+const Joigoose = require('joigoose')(mongoose, null, { timestamps: true })
+const { Joi } = require('koa-joi-router')
+
+const joiSchema = {
+  nickname: Joi.string()
+    .description('用户昵称'),
+
+  username: Joi.string()
+    .description('用户名'),
+
+  mobile: Joi.string().trim()
+    .allow('')
+    .description('手机')
+    .meta({
+      select: false,
+    }),
+
+  wechat_auth: Joi.string().trim()
+    .description('微信授权信息')
+    .meta({
+      select: false,
+    }),
+
+  qq_auth: Joi.string().trim()
+    .description('qq 授权信息')
+    .meta({
+      select: false,
+    }),
+
+  passport: Joi.string()
+    .description('用户密码')
+    .meta({
+      select: false,
+    }),
+
+  email: Joi.string().email()
+    .description('用户邮箱'),
+
+  avatar: Joi.string().trim()
+    .description('用户头像'),
+}
+
+const joi = Joi.object(joiSchema)
+  .options({
+    allowUnknown: true,
+  })
+
+const schema = new mongoose.Schema(Joigoose.convert(joi))
+
+module.exports = mongoose.model('User', schema)
+module.exports.joiSchema = joiSchema
